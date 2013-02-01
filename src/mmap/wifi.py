38ce47bd0f2ac625c77a152ffbf323f8d14149ef
Chase Kernan <chase.kernan@students.olin.edu>
2013-02-01 15:16:07 -0500

    Wifi location cleaned up, working.

--- src/mmap/wifi.py
+++ src/mmap/wifi.py
@@ -2,6 +2,7 @@
 import sys
 import subprocess
 import re
+from functools import total_ordering
 
 class WifiLocator(object):
 
@@ -17,6 +18,30 @@ def make_locator():
 class UnableToScanError(Exception):
     pass
 
+@total_ordering
+class WifiCell(object):
+
+    def __init__(self, address, strength, essid=None):
+        self.address = address
+        self.strength = strength
+        self.essid = essid
+
+    def __repr__(self):
+        return "WifiCell({0}, {1}, {2})"\
+               .format(self.address, self.strength, self.essid)
+
+    def __str__(self):
+        return "Wifi Cell: Address={0}, Strength={1} dBm ESSID={2}"\
+                .format(self.address, self.strength,
+                        self.essid if self.essid else "<UNKNOWN>")
+
+    def __lt__(self, other):
+        return self.strength < other.strength
+
+    def __eq__(self, other):
+        return self.strength == other.strength
+
+
 class BadScanFile(UnableToScanError):
 
     def __init__(self, reason, line_num=None):
@@ -25,16 +50,21 @@ class BadScanFile(UnableToScanError):
 
 class LinuxWifiLocator(WifiLocator):
 
-    def __init__(self, interface='wlan0'):
+    def __init__(self, interface='wlan0', use_sudo=True):
         self.interface = interface
+        self.use_sudo = use_sudo
+        self._parser = _IWListParser()
 
     def get_strengths(self):
         raw_scan = self._get_raw_scan()
-        cells = self._parse_cells(raw_scan)
-        return []
+        cells = self._parser.parse(raw_scan)
+        return sorted(cells, reverse=True) # Best strengths to worst
 
     def _get_raw_scan(self):
         command = ['iwlist', self.interface, 'scan']
+        if self.use_sudo:
+            command.insert(0, 'sudo')
+
         try:
             return subprocess.check_output(command,
                                            stderr=subprocess.STDOUT)
@@ -46,7 +76,6 @@ class _IWListParser(object):
     def parse(self, raw_scan):
         self.lines = raw_scan.splitlines()
         self.line_num = 0
-        print len(self.lines)
 
         self.skip_to_cells()
         cells = []
@@ -57,7 +86,6 @@ class _IWListParser(object):
 
     def skip_to_cells(self):
         while not self.line.startswith('Cell'):
-            print self.line
             self.next()
 
     address = 'Address', re.compile(r"Address: ([0-9A-Z:]*)")
@@ -65,17 +93,23 @@ class _IWListParser(object):
     essid = 'ESSID', re.compile(r'ESSID:"([^"]+)"')
 
     def parse_cell(self):
-        cell = {}
-
-        cell['address'] = self.extract(self.address)
+        address = self.extract(self.address)
         while not self.line.startswith('Quality'): self.next()
-        cell['strength'] = self.extract(self.strength)
+
+        strength = self.extract(self.strength)
         while not self.line.startswith('ESSID'): self.next()
-        cell['essid'] = self.extract(self.essid)
+        try:
+            strength = int(strength)
+        except ValueError:
+            raise BadScanFile('Invalid signal strength: {0}.'.format(strength),
+                              self.line_num)
+
+        essid = self.extract(self.essid)
 
         # Skip the rest
         while self.line and not self.line.startswith('Cell'): self.next()
-        return cell
+
+        return WifiCell(address, strength, essid)
 
     def extract(self, section):
         name, compiled_re = section
