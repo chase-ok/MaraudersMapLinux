5fea8968f98347d4c9f1a6bc32341ff9a3bf1a83
James Nee <therightnee@gmail.com>
2013-02-01 14:45:32 -0500

    Login

---
+++ backend.py
@@ -0,0 +1,30 @@
+import requests
+
+#login
+def login(user, passwrd):
+	cred = {'username': user, 'password': passwrd }
+	l = requests.post("https://olinapps.herokuapp.com/api/exchangelogin", data=cred)
+	user = l.json()
+	sessionid = user['sessionid']
+	return sessionid
+
+#Grab session information
+def start_session(sessionid):
+	key = {'sessionid': sessionid}
+	d =  requests.get('http://directory.olinapps.com/api/people', params=key)
+	raw = d.json()
+	m = requests.get('http://olinapps.com/api/me', params=key)
+	profile  = m.json()
+	fullname = profile['user']['id']
+	fix = fullname.split('.')
+	keycheck = fix[0].title() + " " + fix[1].title()	
+	###Generate a dictionary linking users to their id's
+	parsed = dict()
+	for i in range(len(raw['people'])):
+		parsed[raw['people'][i]['name']] = raw['people'][i]['id']
+
+	return parsed[keycheck]
+
+sessionid = login('jnee', 'B3loved1')
+
+print start_session(sessionid)
